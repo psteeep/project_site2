36f4a442828784b2509e4a07ec96ee629973ed7d
psteeep <psteeep@users.noreply.github.com>
2021-07-18 13:45:31 +0300

    final lesson

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailNew.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewsUpdateNew.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewsDeleteNew.as_view(), name='news-delete')
 ]
-
--- news/views.py
+++ news/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     news = Articles.objects.order_by('date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailNew(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateNew(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteNew(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
 
 
 def create(request):
